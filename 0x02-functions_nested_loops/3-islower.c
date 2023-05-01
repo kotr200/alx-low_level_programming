37bdfe779c92338d622d7fae8ffdcd939b103437
khawla <khawlaelhaboulikotr2003@gmail.com>
2023-05-01 05:31:24 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *_islower - checks for lowercase character
-*Returns 1 if c is lowercase 
+*Returns 1 if c is lowercase
 *Returns 0 otherwise
 *@c:the character in ASCi table
 *Return: 1 for lowercase. 0 for others
