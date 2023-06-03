e4a175853d1ef40d29f382a188f9aba5f90f317a
kotr200 <khawlaelhaboulikotr2003@gmail.com>
2023-06-03 15:23:42 -0700

    up to date

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -2,11 +2,11 @@
 #include <stddef.h>
 
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
- */
+*_strchr - Entry point
+*@s: input
+*@c: input
+*Return: Always 0 (Success)
+*/
 char *_strchr(char *s, char c)
 {
 	int i = 0;
--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
- *
- * Return: dest
- */
+*_strncpy - copy a string
+*@dest: input value
+*@src: input value
+*@n: input value
+*
+*Return: dest
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
