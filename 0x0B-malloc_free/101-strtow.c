b4132db99761638d2f003ce1cd48d6b2e6a5e45b
khawla <khawlaelhaboulikotr2003@gmail.com>
2023-07-11 05:24:44 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
- *
- * Return: number of words
- */
+*count_word - helper function to count the number of words in a string
+*@s: string to evaluate
+*
+*Return: number of words
+*/
 int count_word(char *s)
 {
 	int flag, c, w;
